c54ec15e6fb1852c2c7833870cef8d4b1d26ec0c
KEV <kev789@ya.ru>
2019-10-02 22:27:52 +0300

    версия О только цифры

--- app/src/main/java/ru/kev/ny2016/ny2016_yek/About.java
+++ app/src/main/java/ru/kev/ny2016/ny2016_yek/About.java
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class About extends AppCompatActivity {
-
+    private  TextView tvVersion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        String versionName = BuildConfig.VERSION_NAME;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
     }
