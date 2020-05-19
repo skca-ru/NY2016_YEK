f23df8d5af943b3d74b7c2fb51bd699680f98cb6
KEV <kev789@ya.ru>
2020-05-19 20:41:54 +0300

    2020

--- app/src/main/java/ru/kev/ny2016/ny2016_yek/About.java
+++ app/src/main/java/ru/kev/ny2016/ny2016_yek/About.java
@@ -2,6 +2,7 @@ package ru.kev.ny2016.ny2016_yek;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class About extends AppCompatActivity {
@@ -9,20 +10,24 @@ public class About extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-
         String versionName ="Версия: " +  BuildConfig.VERSION_NAME;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         tvVersion = (TextView) findViewById(R.id.idVersion);
         tvVersion.setText(versionName);
 
+        getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
     @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
